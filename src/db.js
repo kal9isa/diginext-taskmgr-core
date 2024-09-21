44cb138973c1aadf75b14006f895793efce7b8cc
a.salehi.airtour <a.salehi.airtour@gmail.com>
2024-09-21 19:07:40 +0330

    bugfix: setupTables issue, modified some attr to support columns

--- src/db.js
+++ src/db.js
@@ -1,10 +1,36 @@
 const sqlite3 = require('sqlite3').verbose()
+const fs = require('fs')
 
 class Database {
   constructor() {
     if (!Database.instance) {
-      this.db = new sqlite3.Database('./db/task-manager.sqlite')
-      this.setupTables()
+      const dbPath = './db/task-manager.sqlite'
+      const dbFolder = './db'
+
+      // Ensure the db folder exists
+      if (!fs.existsSync(dbFolder)) {
+        fs.mkdirSync(dbFolder)
+      }
+
+      // Check if the '--setup' flag is passed as a console argument
+      const runSetup = process.argv.includes('--setup')
+
+      // If the --setup flag is passed, remove the existing database file
+      if (runSetup && fs.existsSync(dbPath)) {
+        console.log('Removing existing database...')
+        fs.unlinkSync(dbPath) // Remove the previous db file
+      }
+
+      // Initialize the database connection
+      this.db = new sqlite3.Database(dbPath)
+
+      if (runSetup || !fs.existsSync(dbPath)) {
+        console.log('Setting up tables...')
+        this.setupTables()
+      } else {
+        console.log('Database already exists, skipping table setup.')
+      }
+
       Database.instance = this
     }
 
@@ -37,7 +63,6 @@ class Database {
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         title TEXT NOT NULL,
         dateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
-        baseCol TEXT NOT NULL,
         workspace INTEGER NOT NULL,
         FOREIGN KEY (workspace) REFERENCES Workspaces(id) ON DELETE CASCADE
       );`)
@@ -52,10 +77,11 @@ class Database {
         dateCreated DATETIME DEFAULT CURRENT_TIMESTAMP,
         dueDate DATETIME,
         priority TEXT,
-        status TEXT,
         timeEstimate INTEGER,
         timeSpent INTEGER,
+        column INTEGER NOT NULL,
         board INTEGER NOT NULL,
+        FOREIGN KEY (column) REFERENCES Columns(id) ON DELETE CASCADE
         FOREIGN KEY (board) REFERENCES Boards(id) ON DELETE CASCADE
       );`)
   }
@@ -84,11 +110,24 @@ class Database {
       );`)
   }
 
+  setupColumnsTable() {
+    this.db.run(`
+      CREATE TABLE Columns (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        boardId INTEGER NOT NULL,
+        position INTEGER NOT NULL,
+        FOREIGN KEY (boardId) REFERENCES Boards(id) ON DELETE CASCADE
+      );
+    `)
+  }
+
   setupTables() {
     this.db.serialize(() => {
       this.setupUsersTable()
       this.setupWorkspacesTable()
       this.setupBoardsTable()
+      this.setupColumnsTable()
       this.setupTasksTable()
       this.setupUserTaskAssignments()
       this.setupTaskActions()
