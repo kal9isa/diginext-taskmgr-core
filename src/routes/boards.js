5f2a58f8b36115a60d8362f1c6f8dec4408abca9
hamed.nm21 <hamed.nami1994@gmail.com>
2024-10-14 23:29:42 +0330

    fix task list in board API

--- src/routes/boards.js
+++ src/routes/boards.js
@@ -52,16 +52,17 @@ router.get('/:id', (req, res) => {
   const boardQuery = 'SELECT * FROM Boards WHERE id = ?'
   const columnsQuery =
     'SELECT * FROM Columns WHERE boardId = ? ORDER BY position'
-  const tasksQuery = `
+    const tasksQuery = `
     SELECT t.id, t.title AS name, t.dueDate, 
       GROUP_CONCAT(u.username) AS assignees, 
       t.column AS columnId
-      FROM Tasks t
-      LEFT JOIN UserTaskAssignments uta ON t.id = uta.taskId
-      LEFT JOIN Users u ON uta.userId = u.id
-      WHERE t.column = ?
-      GROUP BY t.id
-  `
+    FROM Tasks t
+    LEFT JOIN UserTaskAssignments uta ON t.id = uta.taskId
+    LEFT JOIN Users u ON uta.userId = u.id
+    LEFT JOIN Columns c ON t.column = c.id
+    WHERE c.boardId = ?
+    GROUP BY t.id
+  `;
 
   // Fetch the board data
   db.db.get(boardQuery, [id], (err, board) => {
@@ -84,6 +85,9 @@ router.get('/:id', (req, res) => {
         if (err) {
           return res.status(500).json({ error: err.message })
         }
+
+        console.log(tasks);
+        
 
         // Process tasks to convert the assignees string into an array
         tasks = tasks.map((task) => ({
